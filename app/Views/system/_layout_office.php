d593eeecbd718271df6d0e9148482bb80ba27c3d
Ratinan Lee <lee.ratinan@gmail.com>
2026-01-22 19:32:24 +0800

    remove unused js

--- app/Views/system/_layout_office.php
+++ app/Views/system/_layout_office.php
@@ -130,7 +130,6 @@
 <script src="<?= base_url('assets/vendor/echarts/echarts.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/quill/quill.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/simple-datatables/simple-datatables.js') ?>"></script>
-<script src="<?= base_url('assets/vendor/tinymce/tinymce.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/php-email-form/validate.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/toastrjs/toastr.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/Luxon/luxon.min.js') ?>"></script>
