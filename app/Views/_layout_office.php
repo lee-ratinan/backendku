67f48a95dc58818de4f61afd52775e1ee50ccc37
Ratinan Lee <lee.ratinan@gmail.com>
2026-01-22 19:34:28 +0800

    remove unused js

--- app/Views/_layout_office.php
+++ app/Views/_layout_office.php
@@ -353,7 +353,6 @@
     </div>
 </div>
 <script src="<?= base_url('appstack/js/app.js') ?>"></script>
-<script src="<?= base_url('assets/vendor/tinymce/tinymce.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/toastrjs/toastr.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/Luxon/luxon.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/DataTables/datatables.min.js') ?>"></script>
