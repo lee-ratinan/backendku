381003006177813278dfda13ecdfd2b1f2d6123a
Ratinan Lee <lee.ratinan@gmail.com>
2026-05-27 12:32:51 +0800

    fix dashboard

--- app/Views/system/office_dashboard.php
+++ app/Views/system/office_dashboard.php
@@ -15,7 +15,16 @@ $this->extend($layout);
                     <div class="card-body">
                         <h2>Daily Affirmation</h2>
                         <hr/>
-                        <h3 class="text-center"><i class="fa-solid fa-quote-left"></i> <?= get_daily_affirmation() ?></h3>
+                        <h3 class="text-center my-5"><i class="fa-solid fa-quote-left"></i> <?= get_daily_affirmation() ?></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h2>Add Health Activity</h2>
+                        <a href="<?= base_url($session->locale . '/office/health/activity') ?>" class="btn btn-primary">View All</a>
+                        <a href="<?= base_url($session->locale . '/office/health/activity/new/ejac') ?>" class="btn btn-primary"><i class="fa-solid fa-plus-circle"></i> Add New</a>
                     </div>
                 </div>
             </div>
@@ -25,7 +34,7 @@ $this->extend($layout);
                         <h2>Upcoming Holidays</h2>
                         <?php
                         if (empty($holidays)) {
-                            echo '<p>No upcoming holidays</p>';
+                            echo '<p>No upcoming holidays.</p>';
                         } else {
                             $this_date = '';
                             foreach ($holidays as $holiday) {
@@ -47,19 +56,24 @@ $this->extend($layout);
                 <div class="card">
                     <div class="card-body">
                         <h2>YTD Income</h2>
-                        <?php foreach ($ytd_currencies as $ccy) : ?>
-                            <h4 class="mt-2 mb-0 text-center"><?= $ccy ?></h4>
-                            <div class="row">
-                                <div class="col-6 text-center">
-                                    <h5>Subtotal</h5>
-                                    <?= currency_format($ccy, $ytd_totals[$ccy]['subtotal']) ?>
+                        <?php if (empty($ytd_currencies)) : ?>
+                            <p>No income data available.</p>
+                        <?php else : ?>
+                            <?php foreach ($ytd_currencies as $ccy) : ?>
+                                <h4 class="mt-2 mb-0 text-center"><?= $ccy ?></h4>
+                                <div class="row mb-2">
+                                    <div class="col-6 text-center">
+                                        <h5>Subtotal</h5>
+                                        <?= currency_format($ccy, $ytd_totals[$ccy]['subtotal']) ?>
+                                    </div>
+                                    <div class="col-6 text-center">
+                                        <h5>Total</h5>
+                                        <?= currency_format($ccy, $ytd_totals[$ccy]['total']) ?>
+                                    </div>
                                 </div>
-                                <div class="col-6 text-center">
-                                    <h5>Total</h5>
-                                    <?= currency_format($ccy, $ytd_totals[$ccy]['total']) ?>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                        <a href="<?= base_url($session->locale . '/office/employment/salary') ?>" class="btn btn-primary">View All</a>
                     </div>
                 </div>
             </div>
